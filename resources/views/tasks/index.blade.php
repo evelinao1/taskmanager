cf159c0f988e5631b859d4ff19aa02efb544156a
evelinao1 <66961623+evelinao1@users.noreply.github.com>
2020-11-16 16:41:04 +0200

    bg

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -33,7 +33,7 @@ $tasks = DB::table('tasks')->where('user_id', '=', $id)-> get();
 <div class="col-start-5 col-span-5">
     <p>Sukurti naują užduotį</p>
     <form action="{{route('task.store')}}" method="post">
-    <textarea rows="4" cols="50" name="task">
+    <textarea class="bg-purple-100 rounded-md" rows="4" cols="50" name="task">
 Užduotis...</textarea>
     <input type="hidden" id="user_id" name="user_id" value="{{$user->id}}">
     <x-jet-button class="ml-4">
@@ -46,7 +46,7 @@ Užduotis...</textarea>
 </div>
 <div class="grid grid-cols-12 gap-4 mt-20">
 <div class="col-start-5 col-span-5">
-<table class="table-auto">
+<table class="table-auto bg-purple-200 rounded-md">
   <thead>
   <thead >
   <tr>
